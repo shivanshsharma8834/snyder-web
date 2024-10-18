ebdb1ff4a673d32e269fc890e4be1610b858af5e
Shivansh Sharma <shivansh.report@gmail.com>
2024-10-18 23:30:17 +0530

    "updated featured component"

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,9 +1,22 @@
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
     return (
         <div className={styles.container}>
-            Featured
+            <h1 className={styles.title}>
+                <b>Hey, mouse here!</b> Discover my stories and creative ideas
+            </h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+                </div>
+                <div className={styles.postContainer}>
+                    <h1 className={styles.postTitle}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. architecto </h1>
+                    <p className={styles.postDesc}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti fugit ipsa vero optio officiis accusantium, at quisquam. Eveniet, quo reiciendis.</p>
+                    <button className={styles.button}>Read more</button>
+                </div>
+            </div>
         </div>
     )
 }
